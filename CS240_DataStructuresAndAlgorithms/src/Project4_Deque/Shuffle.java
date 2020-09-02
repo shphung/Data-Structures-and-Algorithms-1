27b7677f8b0b332605b00af149c9807116747e16
Steven <stevenphunghuynh@yahoo.com>
2020-09-02 06:03:19 -0700

    Create Shuffle.java

---
+++ CS240_DataStructuresAndAlgorithms/src/Project4_Deque/Shuffle.java
@@ -0,0 +1,31 @@
+//
+//	Name: 		Phung, Steven
+//	Project:	#4
+//	Due: 		December 4th, 2017
+//	Course: 	cs-240-01-F17
+//
+//	Description:
+//				Implementation of class ArrayStack that is the stack ADT, class Expression that deals with a given expression,
+//				and class ExpressionTest with a main that tests all the methods of Expression and ArrayStack.
+//
+package Project4_Deque;
+/*
+ * One way to shuffle playing cards is to use a perfect shuffle. 
+ * First, you divide a deck of 52 cards into two halves of 26 cards each. 
+ * Next, you merge the halves by interleaving the cards as follows. 
+ * Beginning with the top half and alternating halves, you take the bottom card from a half and place it on top of a new deck. 
+ * For example, if our deck contains the six cards 1 2 3 4 5 6, the top half is 1 2 3, and the bottom half is 4 5 6. 
+ * The 3 at the bottom of the top half becomes the bottom card in the shuffled deck. 
+ * We then place the 6, which is at the bottom of the bottom half, on top of the shuffled deck. 
+ * Next, we place 2 on top, then 5, 1, and finally 4. The shuffled deck is then 4 1 5 2 6 3. 
+ * Notice that the card that was on top of the original deck is now second in the shuffled result, and the bottom card in the original deck is now second from the bottom in the shuffled deck. 
+ * This shuffle is called an in-shuffle and is achieved by beginning with the top half when you move cards into the shuffled result. 
+ * If you begin with the bottom half, you get an out-shuffle, whereby the original top card and bottom card remain in their positions in the shuffled deck. 
+ * Define a class of playing-card decks by using a deque to contain the cards. Your class should define methods to perform perfect in-shuffles and perfect out-shuffles. 
+ * Using your class, 
+ * a. Determine the number of perfect out-shuffles needed to return a deck of n cards to its original order. 
+ * b. Determine the number of perfect in-shuffles needed to return a deck of n cards to its original order.
+ */
+public class Shuffle {
+
+}
